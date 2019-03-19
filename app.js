b820b226f93df6ad94413a0fbf520f056fb779be
Ali Mohamadm <alimohamad1122@gmail.com>
2019-03-18 22:51:29 -0400

    added assorted urls and direct links to creative work.

--- app.js
+++ app.js
@@ -50,10 +50,10 @@ document.getElementById("internships").addEventListener("click", function(){
 
 
 let usacsDetails = [
-    "In the Fall, I coordinated several \"speed dating\" sessions in order to help underclassmen find a mentor in our 200+ student-large undergraduate mentorship program, with the help of our Education Director.",
+    "In the Fall, I coordinated several \"speed dating\" sessions in order to help underclassmen find a mentor in our 200+ student-large undergraduate mentorship program, with the help of our <a href = \"https://maravichandran.github.io\" target=\"_blank\">Education Director.</a>",
     "USACS's Tech Director and I work together to organize Hack Nights every two weeks so that novice students can learn new skills by contributing to social good projects USACS sources from third-party organizations.",
-    "USACS's Outreach Director and I collaborate throughout the year to organize several events with companies looking to recruit, in addition to installments of our CS Speaker Series.",
-    "I help our <a href = \"https://goldencarrots72.github.io/\"><b>Community Director</b></a> run fun social events for Rutgers CS students!",
+    "USACS's <a href = \"http://benyang.me/\" target=\"_blank\">Outreach Director</a> and I collaborate throughout the year to organize several events with companies looking to recruit, in addition to installments of our CS Speaker Series.",
+    "I help our <a href = \"https://goldencarrots72.github.io/\" target=\"_blank\"><b>Community Director</b></a> run fun social events for Rutgers CS students!",
     "I worked with the President of the Rutgers Cognitive Science Club to establish a research pipeline for students interested in REUs, by pairing professors in the CogSci department with undergrads with specific skillsets.",
     "I created a sponsorship document for companies interested in becoming more involved with USACS to buy events packages, which also include sponsorship in HackRU.",
     "I help coordinate new articles for the USACS blog!"
@@ -86,9 +86,9 @@ document.getElementById("hackathons").addEventListener("click", function(){
 
 
 let spDetails = [
-    "In summer 2018, I created an interpreter for a LaTeX-like markup language that I built to generate screenplays. Using basic DOM event listeners, I watched for changes in a textbox, separated the string value of the textbox line by line and pushed each line to a stack, then popped and rendered a given line of markup (if it was character dialogue, a \“slugline\” to identify setting, etc.) inside an iframe.",
+    "In summer 2018, I created an interpreter for <a href = \"https://alimohamad.github.io/Screenplaytex/\" target=\"_blank\">a LaTeX-like markup language that I built to generate screenplays.</a> Using basic DOM event listeners, I watched for changes in a textbox, separated the string value of the textbox line by line and pushed each line to a stack, then popped and rendered a given line of markup (if it was character dialogue, a \“slugline\” to identify setting, etc.) inside an iframe.",
     "In summer 2018, I wrote a Python script to simplify my workflow while programming. It runs os.system() commands executing git add, commit, and push functions, while using the emoji library to print out a guy hitting the whip, since the commit message was always \"wip\", which is short for work-in-progress. I built this on a bus ride from Boston to New Jersey in order to get better at Python which I was using at work.",
-    "In 2017, I won 3rd Place Overall at PennApps XV! My team and I built a desktop accessibility tool for Parkinson's disease patients called TactEYEle - we used OpenCV to track the movement of the user's head and move the cursor accordingly, and correspond winks and blinks to clicks. Using IBM Watson's Speech-to-Text API for voice recognition, voice commands such as \"select\", \"copy / paste\", and so on correspond to the keyboard shortcuts being carried out, whereas \"transcribe\" followed by a phrase types that phrase out.",
+    "In 2017, I won <a href=\"https://youtu.be/5IFfr-ggy-8?t=23m11s\" target=\"_blank\">3rd Place Overall at PennApps XV!</a> My team and I built a desktop accessibility tool for Parkinson's disease patients called TactEYEle - we used OpenCV to track the movement of the user's head and move the cursor accordingly, and correspond winks and blinks to clicks. Using IBM Watson's Speech-to-Text API for voice recognition, voice commands such as \"select\", \"copy / paste\", and so on correspond to the keyboard shortcuts being carried out, whereas \"transcribe\" followed by a phrase types that phrase out.",
     "In summer 2017, I was the sole backend developer for a leaderboard for the GaryVee Flip Challenge, an internet challenge Gary Vaynerchuck posed to his YouTube audience to go out and flip items they got at garage sales in order to make $20,170 in 2017. I built the whole thing in Meteor.js."
 ];
 
@@ -104,10 +104,10 @@ document.getElementById("side-projects").addEventListener("click", function(){
 
 
 let cpDetails = [
-    "I take a lot of photos. Look at them here!",
-    "In Summer 2017, I started a Rutgers-themed Anti Social Social Club parody called Anti Student Student Club. I coordinated all marketing material, and sell the sweaters out of my room. Check out the lookbook!",
-    "In Fall 2018, I took Screenwriting for Television as a part of my Creative Writing minor at Rutgers. Here's a scene from the show I created that semester, \"Woke\". ",
-    "In Spring 2019, I took Digital Composition for my Creative Writing minor, where I worked on a bunch of different media projects for the internet. Here's a short two-minute podcast I did on the history of Rutgers's experimental Livingston Residential College."
+    "I take a lot of photos. Look at them <a href = \"https://vsco.co/alaiyo/\" target=\"_blank\">here!</a>",
+    "In Summer 2017, I started a Rutgers-themed Anti Social Social Club parody called Anti Student Student Club. I coordinated all marketing material, and sell the sweaters out of my room. Check out the <a href = \"https://www.instagram.com/antistudentstudentclub/\" target=\"_blank\">lookbook!</a>",
+    "In Fall 2018, I took Screenwriting for Television as a part of my Creative Writing minor at Rutgers. Here's a scene from the show I created that semester, <a href = \"assets/SCENE.pdf\" target=\"_blank\">\"Woke\".</a> ",
+    "In Spring 2019, I took Digital Composition for my Creative Writing minor, where I worked on a bunch of different media projects for the internet. Here's a short two-minute podcast I did on the history of Rutgers's experimental <a href = \"assets/Ali - Livingston Residential College.wav\" target=\"_blank\">Livingston Residential College."
     
 ];
 
